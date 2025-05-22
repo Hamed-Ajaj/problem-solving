bff19d93a2a1dfa4517a2aef1284d893140ffe26
Hamed-Ajaj <hamedsgaming@gmail.com>
2025-05-22 15:52:24 +0300

    adding days and years

--- formatTime.js
+++ formatTime.js
@@ -1,4 +1,5 @@
 const formatTime = (seconds) => {
+  if (seconds === 0) return "now";
   const joinWithAnd = (arr) => {
     if (arr.length === 0) return "";
     if (arr.length === 1) return arr[0];
@@ -7,10 +8,14 @@ const formatTime = (seconds) => {
   };
 
   if (seconds < 1) return `${seconds} second`;
-  const h = Math.floor(seconds / 3600);
+  const y = Math.floor(seconds / 31556952);
+  const d = Math.floor(seconds / 86400) % 365;
+  const h = Math.floor((seconds / 3600) % 24);
   const m = Math.floor((seconds / 60) % 60);
   const s = seconds % 60;
   const parts = [];
+  if (y > 0) parts.push(`${y} ${y === 1 ? "year" : "years"}`);
+  if (d > 0) parts.push(`${d} ${d === 1 ? "day" : "days"}`);
   if (h > 0) parts.push(`${h} ${h === 1 ? "hour" : "hours"}`);
   if (m > 0) parts.push(`${m} ${m === 1 ? "minute" : "minutes"}`);
   if (s > 0 || parts.length === 0)
@@ -19,4 +24,4 @@ const formatTime = (seconds) => {
   return joinWithAnd(parts);
 };
 
-console.log(formatTime(50));
+console.log(formatTime(132030240));
